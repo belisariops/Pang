867eb6512e9d12b4af9f92eda5b09e5bf87009d3
belisariops <belisariops@gmail.com>
2016-11-26 01:58:37 -0300

    Regresion

---
+++ cmake-build-debug/CMakeFiles/Patterns.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/BSPCut.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/BSPCut.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/BSPNode.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/BSPNode.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/BSPTree.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/BSPTree.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/BackGround.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/BackGround.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/Ball.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/Ball.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/BallCollider.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/BallCollider.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/BlueBallModel.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/BlueBallModel.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/BoxCollider.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/BoxCollider.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/BoxHorizontal.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/BoxHorizontal.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/BoxVertical.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/BoxVertical.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/Collider.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/Collider.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/GameDriver.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/GameDriver.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/GroundCollider.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/GroundCollider.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/LTexture.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/LTexture.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/LargeBall.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/LargeBall.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/MediumBall.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/MediumBall.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/Point.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/Point.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/QuadTreeNode.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/QuadTreeNode.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/RedBallModel.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/RedBallModel.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/Segment.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/Segment.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/SmallBall.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/SmallBall.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/State.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/State.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/Timer.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/Timer.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/Vector.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/Vector.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/Velocity2D.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/Velocity2D.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/View.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/View.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/WallCollider.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/WallCollider.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/Window.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/Window.cpp.o"
+  "/home/belisariops/WorkSpace/C++/SDL2/Pang/main.cpp" "/home/belisariops/WorkSpace/C++/SDL2/Pang/cmake-build-debug/CMakeFiles/Patterns.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Patterns.dir/cmake_clean.cmake
@@ -0,0 +1,38 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Patterns.dir/main.cpp.o"
+  "CMakeFiles/Patterns.dir/Ball.cpp.o"
+  "CMakeFiles/Patterns.dir/LargeBall.cpp.o"
+  "CMakeFiles/Patterns.dir/MediumBall.cpp.o"
+  "CMakeFiles/Patterns.dir/State.cpp.o"
+  "CMakeFiles/Patterns.dir/Timer.cpp.o"
+  "CMakeFiles/Patterns.dir/RedBallModel.cpp.o"
+  "CMakeFiles/Patterns.dir/LTexture.cpp.o"
+  "CMakeFiles/Patterns.dir/View.cpp.o"
+  "CMakeFiles/Patterns.dir/Window.cpp.o"
+  "CMakeFiles/Patterns.dir/SmallBall.cpp.o"
+  "CMakeFiles/Patterns.dir/BackGround.cpp.o"
+  "CMakeFiles/Patterns.dir/GameDriver.cpp.o"
+  "CMakeFiles/Patterns.dir/Collider.cpp.o"
+  "CMakeFiles/Patterns.dir/Point.cpp.o"
+  "CMakeFiles/Patterns.dir/Segment.cpp.o"
+  "CMakeFiles/Patterns.dir/Vector.cpp.o"
+  "CMakeFiles/Patterns.dir/GroundCollider.cpp.o"
+  "CMakeFiles/Patterns.dir/WallCollider.cpp.o"
+  "CMakeFiles/Patterns.dir/BallCollider.cpp.o"
+  "CMakeFiles/Patterns.dir/BoxCollider.cpp.o"
+  "CMakeFiles/Patterns.dir/BoxVertical.cpp.o"
+  "CMakeFiles/Patterns.dir/BoxHorizontal.cpp.o"
+  "CMakeFiles/Patterns.dir/Velocity2D.cpp.o"
+  "CMakeFiles/Patterns.dir/BSPTree.cpp.o"
+  "CMakeFiles/Patterns.dir/BSPNode.cpp.o"
+  "CMakeFiles/Patterns.dir/BSPCut.cpp.o"
+  "CMakeFiles/Patterns.dir/QuadTreeNode.cpp.o"
+  "CMakeFiles/Patterns.dir/BlueBallModel.cpp.o"
+  "Patterns.pdb"
+  "Patterns"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Patterns.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
