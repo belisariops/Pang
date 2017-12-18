e9e8294a47f3ccbae86015a88430e1b128b913b8
Belisario Panay <belisariops@gmail.com>
2017-12-17 23:34:23 -0300

    Fixed cmake file, also deleted some unused files

--- main.cpp
+++ main.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 #include <SDL2/SDL.h>
-#include <sstream>
 #include "Window.h"
 #include "GameDriver.h"
-#include "Timer.h"
 
 using namespace std;
 
 int main() {
     Window* game = Window::getInstance();
     SDL_Event e;
-    GameDriver* gameDriver = new GameDriver();
+    GameDriver gameDriver = GameDriver();
     Timer fpsTimer;
     Timer capTimer;
-    int countedFrames=0;
     fpsTimer.start();
 
     while (!game->getQuitState()){
@@ -27,25 +24,14 @@ int main() {
             }
 
             if ( e.type == SDL_KEYDOWN){
-                gameDriver->spawn();
+                gameDriver.spawn();
             }
 
 
         }
-        float avgFPS = countedFrames / ( fpsTimer.getTicks() / 1000.f );
 
-        //Set text to be rendered
-
-        //cout << game->getScreenTicks()  << "\n";
-        if (fpsTimer.getTicks() >10) {
-            cout << avgFPS << "\n";
-            cout << gameDriver->getCount() << "\n";
-        }
-
-
-        gameDriver->notify();
+        gameDriver.notify();
         game->update();
-        ++countedFrames;
         //If frame finished early
         int frameTicks = capTimer.getTicks();
         if( frameTicks < game->getScreenTicks() )
@@ -53,15 +39,8 @@ int main() {
             //Wait remaining time
             SDL_Delay( game->getScreenTicks() - frameTicks );
         }
-        //cout<< capTimer.getTicks()<<endl;
-        //cout<<fpsTimer.getTicks()<<"\n";
-        if (fpsTimer.getTicks() >1000.f){
-            fpsTimer.start();
-            countedFrames=0;
-        }
 
     }
-    delete gameDriver;
     delete game;
     return 0;
 }
